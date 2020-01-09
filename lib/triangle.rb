c56204a78531f4ce3faa00aa199cc44de596c3f9
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-09 00:12:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,8 +2,7 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(attribute)
-    	5. attributes.each {|key, value| self.send(("#{key}="), value)}
-
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
   
 end
